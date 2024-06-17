c1e3aded31d0c662ebca287755e170cd6ad1b0d4
Ryan Beck <ryan@ryanbeck.io>
2024-06-17 16:55:41 +0100

    Code refactor for readablity

--- Memory Game/ContentView.swift
+++ Memory Game/ContentView.swift
@@ -10,22 +10,62 @@ import SwiftData
 import Foundation
 
 struct ContentView: View {
-    let emojis = ["😂", "😏", "😇", "🥹"] // Array for emoji display
+    let emojis = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐻‍❄️", "🐨"] // Array for emoji display
+    
+    @State var cardCount: Int = 4 // Set the card count to display 4 initially
+    // State set to track number of cards and increase/decrease
+    
     var body: some View {
+        VStack {
+            cardDisplay
+            cardCountAdjuster
+        }
+        .padding()
+    }
+    
+    var cardDisplay: some View {
         HStack {
-            ForEach(emojis.indices, id: \.self) { index in
+            ForEach(0..<cardCount, id: \.self) { index in // from 0 to the card count value
                 CardView(content: emojis[index])
             } // For each entry in the emojis array, create a card
         }
         .foregroundColor(.orange)
-        .padding()
-        
+    }
+    
+    var cardCountAdjuster: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.medium)
+        .font(.largeTitle)
+    }
+    
+    var cardRemover: some View {
+        Button (action: {
+            if cardCount > 1 { // Prevent removing cards past 1
+                cardCount -= 1 // Remove a card from the display
+            }
+        }, label: {
+            Image(systemName: "minus")
+        })
+    }
+    
+    var cardAdder: some View {
+        Button (action: {
+            if cardCount < emojis.count { // Only add the amount of cards that are in the array
+                cardCount += 1 // Add a card to the display
+            }
+        }, label: {
+            Image(systemName: "plus")
+        })
     }
 }
 
 struct CardView: View {
     let content: String // Force variable content to display the entered string
-    @State var isFaceUp = false // Default to face down
+    @State var isFaceUp = true // Default to face up
     
     var body: some View {
         ZStack {
